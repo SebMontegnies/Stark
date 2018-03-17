d320afebde71fd9c7ea5951a769bbbc47fcdc696
Sebastien Montegnies <sebastien.montegnies@editushome.lu>
2018-03-17 09:51:38 +0100

    General information

---
+++ WebApp/WebApp/ViewModels/GeneralInformationViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.ViewModels
+{
+	public class GeneralInformationViewModel
+	{
+		public Gender Gender { get; set; }
+		public int Age { get; set; }
+		public bool HasChildren { get; set; }
+		public int Heartbeat { get; set; }
+		public decimal Temperature { get; set; }
+		public decimal BloodoOxygenationRate { get; set; }
+	}
+}
