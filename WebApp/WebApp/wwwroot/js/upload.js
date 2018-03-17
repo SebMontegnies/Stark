0a53764c6030aac903b9473252e9c80b7f2cacb3
Damien Borgniet <damien.borgniet@editushome.lu>
2018-03-17 09:10:29 +0100

    dev

--- WebApp/WebApp/wwwroot/js/upload.js
+++ WebApp/WebApp/wwwroot/js/upload.js
@@ -10,16 +10,32 @@
 
 	$('#file').change(function () {
 		$('#file-name').text($('#file')[0].files[0].name);
-		if ($('#file')[0].files[0].name != '') {
+		file = $('#file')[0].files[0];
+		if (file !== null) {
 			$('#file-loader').css({ 'display': 'inline-block' });
+	
+			var reader = new FileReader();
+			reader.onload = function () {
+				$.ajax({
+					url: '/attachmentURL',
+					type: 'POST',
+					data: reader.result
+				});
+			}
+			console.log(reader.readAsBinaryString(file));
+			//$.ajax({
+			//	type: "POST",
+			//	url: "http://localhost:2921/api/FaceRecognition",
+			//	data: { data: data },
+			//	success: function (result) {
 
-			$.ajax({
-				type: "POST",
-				url: "http://localhost:2921/api/FaceRecognition",
-				data: { data: imageData },
-				success: function (result) {
-				}
-			});
+			var documentName = '<span>' + file.name + '</span>';
+			var symptomResult = 'Souffle au coeur';
+			var symptom = '<span>' + symptomResult + '</span>'; //result.symptom
+			$('#files-list').append('<div class="file">' + documentName + symptom + '</div>');
+
+			//	}
+			//});
 		}
 	});
 
