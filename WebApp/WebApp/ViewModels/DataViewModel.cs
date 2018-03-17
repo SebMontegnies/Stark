3d3ff3a2cdad63978a9a7aef0d5888b6702e21a1
Sebastien Montegnies <sebastien.montegnies@editushome.lu>
2018-03-17 07:53:56 +0100

    Data

---
+++ WebApp/WebApp/ViewModels/DataViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.ViewModels
+{
+	public class DataViewModel
+	{
+		public double Temperature { get; set; }
+		public double Hearbeat { get; set; }
+		public double BloodoxygenationRate { get; set; }
+	}
+}
