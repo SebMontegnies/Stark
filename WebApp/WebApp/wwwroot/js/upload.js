124f150c577f339996adc079298d3dc8f1ff0a43
Damien Borgniet <damien.borgniet@editushome.lu>
2018-03-17 15:06:31 +0100

    dev

--- WebApp/WebApp/wwwroot/js/upload.js
+++ WebApp/WebApp/wwwroot/js/upload.js
@@ -1,33 +1,29 @@
-﻿function getBase64(file) {
-	var reader = new FileReader();
-	reader.readAsDataURL(file);
-	reader.onload = function () {
-		return reader.result;
-	};
-	reader.onerror = function (error) {
-		console.log('Error: ', error);
-	};
-}
-
-$(function () {
+﻿$(function () {
 
 	$('#file').change(function () {
 		$('#file-name').text($('#file')[0].files[0].name);
 		file = $('#file')[0].files[0];
 		if (file !== null) {
 			$('#file-loader').css({ 'display': 'inline-block' });
-	
-			 
+			var imageBase64;
+			var reader = new FileReader();
+			reader.readAsDataURL(file);
+			reader.onload = function () {
+				imageBase64 = reader.result;
+			};
+			reader.onerror = function (error) {
+				console.log('Error: ', error);
+			};
 			$.ajax({
 				type: "POST",
-				url: "http://localhost:8000/api/File",
-				data: { data: getBase64(file) },
+				url: "/api/File",
+				data: { data: imageBase64 },
 				success: function (result) {
-					console.log(result);
+					$('.question.file').hide();
 					var documentName = '<span>' + file.name + '</span>';
-					var symptomResult = result;
-					var symptom = '<span>' + symptomResult + '</span>'; //result.symptom
-					$('#files-list').append('<div class="file">' + symptom + '<br /><span class="file-name">' + documentName + '</span></div>');
+					var symptom = '<span>' + result + '</span>';
+					$('#files-list').append('<img src="' + imageBase64 + '" width="320" />');
+					$('#files-list').append('<span class="symptom">The system identifies this pathology : <strong>' + symptom + '<strong></span></div><br />');
 				}
 			});
 		}
