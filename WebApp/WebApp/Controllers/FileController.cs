15ec78a0baf5a939ab7848f5dc382194f62a8caf
Sebastien Montegnies <sebastien.montegnies@editushome.lu>
2018-03-17 08:47:58 +0100

    Get file

---
+++ WebApp/WebApp/Controllers/FileController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/File")]
+    public class FileController : Controller
+    {
+        // GET: api/File
+        [HttpGet]
+        public string Get(byte[] bytes)
+        {
+	        return "test";
+        }
+    }
+}
